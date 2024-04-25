16f735d8fb86f0b9fbb410b9293ca0a4511d6339
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2024-04-26 01:07:31 +0530

    created button for slack auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import classes from "./App.module.css";
+import { useState } from "react";
+const BASE_URL = "http://localhost:8080/oauth/signin";
 
 function App() {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const authorizeSlackHandler = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+
+      if (response.data.ok) {
+        setIsAuthorized(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className={classes.app}>
+      {isAuthorized && <p>You are connected to slack!</p>}
+      {!isAuthorized && (
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className={classes.button}
+          href={BASE_URL}
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Authorize Slack
         </a>
-      </header>
+      )}
     </div>
   );
+  // <button onClick={authorizeSlackHandler}>Authorize Slack</button>
 }
 
 export default App;
